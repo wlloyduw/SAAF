afd508eec1645a13224bed98d42362ac264d986a
Wes Lloyd <wlloyd@uw.edu>
2018-11-01 23:53:05 -0700

    restored placeholders

--- lambda/java_template/test/callservice.sh
+++ lambda/java_template/test/callservice.sh
@@ -4,8 +4,7 @@
 json={"\"name\"":"\"Fred\u0020Smith\",\"param1\"":1,\"param2\"":2,\"param3\"":3}
 
 echo "Invoking Lambda function using API Gateway"
-curl -s -H "Content-Type: application/json" -X POST -d  $json https://523zh3cjpd.execute-api.us-east-1.amazonaws.com/hello_dev
-#time output=`curl -s -H "Content-Type: application/json" -X POST -d  $json https://523zh3cjpd.execute-api.us-east-1.amazonaws.com/hello_dev`
+time output=`curl -s -H "Content-Type: application/json" -X POST -d  $json {INSERT API GATEWAY URL HERE}`
 
 echo ""
 echo "CURL RESULT:"
@@ -14,7 +13,7 @@ echo ""
 echo ""
 
 echo "Invoking Lambda function using AWS CLI"
-time output=`aws lambda invoke --invocation-type RequestResponse --function-name hello --region us-east-1 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
+time output=`aws lambda invoke --invocation-type RequestResponse --function-name {INSERT AWS FUNCTION NAME HERE} --region us-east-1 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
 echo ""
 echo "AWS CLI RESULT:"
 echo $output
