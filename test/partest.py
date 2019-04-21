b0ecf50ef16f50f17e0b72ef117176b2fed905ae
Robert Cordingly <rcording@uw.edu>
2019-04-20 21:28:40 -0700

    Minor updates

--- test/partest.py
+++ test/partest.py
@@ -10,8 +10,9 @@ import json
 import datetime
 from decimal import Decimal
 
+url_list = ['https://2q0ng575ue.execute-api.us-east-1.amazonaws.com/calcservice_dev/', 'https://4utqpw7zhb.execute-api.us-east-1.amazonaws.com/DEPLOY']
 url_list = ['https://2q0ng575ue.execute-api.us-east-1.amazonaws.com/calcservice_dev/']
-payload = {'threads':2,'calcs':500,'loops':10000,'sleep':0}
+payload = {'threads':2,'calcs':500,'loops':500,'sleep':0}
 headers = {'content-type':'application/json'}
 
 #
@@ -24,21 +25,26 @@ runs_per_thread = int(total_runs / threads)
 run_results = []
 
 #
-# Print starter information
+# Define a function to be called by each thread.
 #
-print(datetime.datetime.now())
-print("Setting up test: runsperthread=" + str(runs_per_thread) + " threads=" + str(threads) + " totalruns=" + str(total_runs))
-
-# Define a function for the thread
-def make_call( thread_id, runs):
+def make_call( thread_id, runs, url):
 	for i in range(0, runs):	
-		for j in range(len(url_list)):
-			response = requests.post(url_list[j], data=json.dumps(payload), headers=headers)
-			dictionary = ast.literal_eval(response.text)
-			dictionary['2_thread_id'] = thread_id
-			dictionary['1_run_id'] = i
-			if 'version' in dictionary:
-				run_results.append(dictionary)
+		response = requests.post(url, data=json.dumps(payload), headers=headers)
+		dictionary = ast.literal_eval(response.text)
+		dictionary['2_thread_id'] = thread_id
+		dictionary['1_run_id'] = i
+		
+		if (len(url_list)) > 1 and 'platform' not in dictionary:
+			dictionary['endpoint'] = url
+		
+		if 'version' in dictionary:
+			run_results.append(dictionary)
+			print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+			print("Run Complete: (" + str(i + 1) + "/" + str(runs) + "):\n")
+			print(dictionary)
+		else:
+			print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+			print("Run Failed: (" + str(i + 1) + "/" + str(runs) + "):")
 		
 #
 # Create a bunch of threads and run make_call.
@@ -46,13 +52,23 @@ def make_call( thread_id, runs):
 try:
 	threadList = []
 	for i in range(0, threads):
-		thread = Thread(target = make_call, args = (i, runs_per_thread))
-		thread.start()
-		threadList.append(thread)
-	for i in range(0, threads):
+		for j in range(len(url_list)):
+			thread = Thread(target = make_call, args = (i, runs_per_thread, url_list[j]))
+			thread.start()
+			threadList.append(thread)
+	for i in range(len(threadList)):
 		threadList[i].join()
+		print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+		print("Threads complete: (" + str(i + 1) + "/" + str(threads) + ").")
 except:
-	print("Error: unable to start thread")
+	pass
+	
+#
+# Print starter information
+#
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+print(datetime.datetime.now())
+print("Setting up test: runsperthread=" + str(runs_per_thread) + " threads=" + str(threads) + " totalruns=" + str(total_runs), " payload=" + str(payload))
 
 #
 # Print results of each run.
@@ -80,7 +96,7 @@ print("Successful Runs: " + str(len(run_results)))
 # Build new dictionaries for each category.
 # key_map defines the dictionary to put runs with unique keys into.
 #
-key_map = {'uuid': {}, 'cpuType': {}, 'vmuptime': {}, 'newcontainer': {}, 'platform': {}, 'lang': {}, 'version': {}}
+key_map = {'uuid': {}, 'cpuType': {}, 'vmuptime': {}, 'newcontainer': {}, 'platform': {}, 'lang': {}, 'version': {}, 'endpoint': {}}
 master_key_list = list(key_map.keys())
 for i in range(len(run_results)):
 	run = run_results[i]
@@ -91,7 +107,7 @@ for i in range(len(run_results)):
 				key_map[target_key][run[target_key]] = [run]
 			else:
 				key_map[target_key][run[target_key]].append(run)
-			
+						
 #
 # Loop through every dictionary created previously.
 # Find values that can be parsed into doubles and calculate the average of them.
@@ -107,7 +123,6 @@ for i in range(len(master_key_list)):
 		run_dict = key_map[key_value][sub_key_list[0]][0]
 		run_attributes = list(run_dict.keys())
 		run_attributes.sort()
-		
 		number_attributes = []
 		
 		for i in range(len(run_attributes)):
@@ -124,21 +139,15 @@ for i in range(len(master_key_list)):
 		print(csv_header)
 		
 		for i in range(len(sub_key_list)):
-			
 			run_list = key_map[key_value][sub_key_list[i]]
-			
 			line = str(sub_key_list[i]) + "," + str(len(run_list)) + ","
-			
 			for j in range(len(number_attributes)):
 				total_value = 0
 				attribute = number_attributes[j]
-				
 				for k in range(len(run_list)):
 					run_dict = run_list[k]
 					total_value += Decimal(run_dict[attribute])
-					
 				line += str(round((total_value / len(run_list)), 2)) + ","
-			
 			line = line[:-1]
 			print(line)
 		print("Runs with unique attribute " + str(key_value) + ": " + str(len(sub_key_list)))
