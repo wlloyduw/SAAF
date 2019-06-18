f839f58e96b053e606b4f7068a39800b8c38afea
Robert Cordingly <rcording@uw.edu>
2019-06-17 21:14:55 -0700

    AWS, Google, and IBM functions can now be instantly deployed without having to set anything up in their websites. Azure still requires you to make a functionApp.

--- nodejs_template/tools/publish.sh
+++ nodejs_template/tools/publish.sh
@@ -27,6 +27,7 @@
 # Get the function name from the config.json file.
 function=`cat ./config.json | jq '.functionName' | tr -d '"'`
 functionApp=`cat ./config.json | jq '.azureFunctionApp' | tr -d '"'`
+lambdaRole=`cat ./config.json | jq '.lambdaRoleARN' | tr -d '"'`
 
 echo
 echo Deploying $function....
@@ -51,14 +52,15 @@ then
 	mkdir build
 	mkdir build/node_modules
 	
-	# Copy files to build folder and create zip.
+	# Copy files to build folder.
 	cp -R ../src/* ./build
 	cp -R ../platforms/aws/* ./build
 	cp -R ../tools/node_modules/* ./build/node_modules
-	zip -X -r ./build/index.zip ./build/*
 	
-	# Submit to AWS Lambda
+	# Zip and submit to AWS Lambda.
 	cd ./build
+	zip -X -r ./index.zip *
+	aws lambda create-function --function-name $function --runtime nodejs8.10 --role $lambdaRole --handler index.js --zip-file fileb://index.zip
 	aws lambda update-function-code --function-name $function --zip-file fileb://index.zip
 	aws lambda update-function-configuration --function-name $function --memory-size $memory --runtime nodejs8.10
 	cd ..
@@ -76,14 +78,14 @@ then
 	mkdir build
 	mkdir build/node_modules
 	
-	# Copy files to build folder and create zip.
+	# Copy files to build folder.
 	cp -R ../src/* ./build
 	cp -R ../platforms/ibm/* ./build
 	cp -R ../tools/node_modules/* ./build/node_modules
-	zip -X -r ./build/index.zip ./build/*
 	
-	# Submit to IBM Cloud Functions
+	# Zip and submit to IBM Cloud Functions.
 	cd ./build
+	zip -X -r ./index.zip *
 	ibmcloud fn action update $function --kind nodejs:8 --memory $memory index.zip
 	cd ..
 fi
