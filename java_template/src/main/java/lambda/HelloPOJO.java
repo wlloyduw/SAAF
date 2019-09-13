29879452fed0d7f0c4527f66ce646895dfe74b1c
Robert Cordingly <rcording@uw.edu>
2019-09-12 19:12:24 -0700

    Added Hashmap Java template variant.

--- java_template/src/main/java/lambda/HelloPOJO.java
+++ java_template/src/main/java/lambda/HelloPOJO.java
@@ -12,28 +12,28 @@ import java.util.HashMap;
  * @author Wes Lloyd
  * @author Robert Cordingly
  */
-public class Hello implements RequestHandler<HashMap<String, Object>, HashMap<String, Object>> {
+public class HelloPOJO implements RequestHandler<Request, HashMap<String, Object>> {
 
     /**
      * Lambda Function Handler
      * 
-     * @param request Hashmap containing request JSON attributes.
+     * @param request Request POJO with defined variables from Request.java
      * @param context 
      * @return HashMap that Lambda will automatically convert into JSON.
      */
-    public HashMap<String, Object> handleRequest(HashMap<String, Object> request, Context context) {
+    public HashMap<String, Object> handleRequest(Request request, Context context) {
         
         //Collect inital data.
         Inspector inspector = new Inspector();
         inspector.inspectAll();
         
         //Add custom message to FaaS Inspector.
-        inspector.addAttribute("message", "Hello " + request.get("name")
+        inspector.addAttribute("message", "Hello " + request.getName() 
                 + "! This is an attributed added to the Inspector!");
         
         //Use and consome a response object. (OPTIONAL)
         Response response = new Response();
-        response.setValue("Hello " + request.get("name")
+        response.setValue("Hello " + request.getName() 
                 + "! This is from a response object!");
         
         inspector.consumeResponse(response);
---
+++ java_template/src/main/java/local/Local.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package local;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author robertcordingly
+ */
+public class Local {
+    
+    // int main enables testing function from cmd line
+    public static void main(String[] args) {
+        // Context c = new Context() {
+        //     @Override
+        //     public String getAwsRequestId() {
+        //         return "";
+        //     }
+
+        //     @Override
+        //     public String getLogGroupName() {
+        //         return "";
+        //     }
+
+        //     @Override
+        //     public String getLogStreamName() {
+        //         return "";
+        //     }
+
+        //     @Override
+        //     public String getFunctionName() {
+        //         return "";
+        //     }
+
+        //     @Override
+        //     public String getFunctionVersion() {
+        //         return "";
+        //     }
+
+        //     @Override
+        //     public String getInvokedFunctionArn() {
+        //         return "";
+        //     }
+
+        //     @Override
+        //     public CognitoIdentity getIdentity() {
+        //         return null;
+        //     }
+
+        //     @Override
+        //     public ClientContext getClientContext() {
+        //         return null;
+        //     }
+
+        //     @Override
+        //     public int getRemainingTimeInMillis() {
+        //         return 0;
+        //     }
+
+        //     @Override
+        //     public int getMemoryLimitInMB() {
+        //         return 0;
+        //     }
+
+        //     @Override
+        //     public LambdaLogger getLogger() {
+        //         return new LambdaLogger() {
+        //             @Override
+        //             public void log(String string) {
+        //                 System.out.println("LOG:" + string);
+        //             }
+        //         };
+        //     }
+        // };
+
+        // // Create an instance of the class
+        // Hello lt = new Hello();
+
+        // // Create a request object
+        // Request req = new Request();
+
+        // // Grab the name from the cmdline from arg 0
+        // String name = (args.length > 0 ? args[0] : "");
+
+        // // Load the name into the request object
+        // req.setName(name);
+
+        // // Report name to stdout
+        // System.out.println("cmd-line param name=" + req.getName());
+
+        // Run the function
+        //HashMap<String, Object> resp = lt.handleRequest(req, c);
+        
+        // Print out function result
+        //System.out.println("function result:" + resp.toString());
+    }
+}
