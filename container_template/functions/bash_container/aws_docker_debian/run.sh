a957e49ef472efa8920072e1dbb7aab8c501b102
wlloyduw <wlloyd@uw.edu>
2024-11-21 02:02:11 -0800

    Update run.sh
    comment out second json assignment

--- container_template/functions/bash_container/aws_docker_debian/run.sh
+++ container_template/functions/bash_container/aws_docker_debian/run.sh
@@ -8,7 +8,7 @@ function=$(jq '.function_name' < ./config.json | tr -d '"')
 profile=$(jq '.profile' < ./config.json | tr -d '"')
 export AWS_PROFILE=$profile
 
-json=$2
+#json=$2
 
 response=$(aws lambda invoke --invocation-type RequestResponse --cli-binary-format raw-in-base64-out --cli-read-timeout 900 --function-name "$function" --payload "$json" /dev/stdout)
 echo "$response" | jq 
